161eaefd6e9c33ccad08430b2aac8d0d020e3240
Amodjha704 <amodkumar0808@gmail.com>
2024-09-12 09:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello this page has been build using S2I feature of OpenShift! We are testing webhook now 2."
+    return "Hello this page has been build using S2I feature of OpenShift! We are testing webhook now 3."
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=8080)
